875c3fb87e5aba859ba2dfb2afa83d5f14e6d9d5
Hernan Y.Ke <yuriqiao@gmail.com>
2016-12-20 14:23:53 -0500

    Add keyword

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,8 +12,8 @@ import { Component,trigger,state,style,transition,animate,keyframes } from '@ang
       state('large',style({
         transform:'scale(1.4)'
       })),
-      transition('small=>large',animate('500ms')),
-      transition('large=>small',animate('500ms'))
+      transition('small=>large',animate('500ms ease-in')),
+      transition('large=>small',animate('500ms ease-out'))
     ])
   ]
 })
