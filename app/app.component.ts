8f38b2c83ecbb9fa06275b9f76c11a4584b7b21d
Hernan Y.Ke <yuriqiao@gmail.com>
2016-12-20 16:10:46 -0500

    Multi-step Animations

--- app/app.component.ts
+++ app/app.component.ts
@@ -32,12 +32,18 @@ import { Component,trigger,state,style,transition,animate,keyframes } from '@ang
         transform:'scale(1.4)'
       })),
       state('fadeIn',style({
-        opacity:'1'
+        opacity:'1',
+        transform:'scale(1.2)'
       })),
       //transition('small<=>large',animate('500ms'))
       transition('void=>*',[
-        style({opacity:'0',transform:'translateY(20px)'}),
-        animate('500ms 3s')
+        //style({opacity:'0',transform:'translateY(20px)'}),
+        //animate('500ms 3s')
+        animate(500,keyframes([
+          style({opacity:0,transform:'translateY(-30px)',offset:0}),
+          style({opacity:1,transform:'translateY(5px) scale(1.2)',offset:0.3}),
+          style({opacity:0.3,transform:'translateY(0px) scale(1.2)',offset:1}),
+        ]))
       ])
 ])
   ]
